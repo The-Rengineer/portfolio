3e3737e68bcab9735d4f999c5049bb5ffe8042aa
Jack9145 <toikakunie@gmail.com>
2022-02-01 21:58:39 +0900

    コンフリクト確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def top
   end
+
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
+    redirect_to("/users/top")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get "users/top" => "users#top"
+get "users/new" => "users#new"
+post "users/create" => "users#create"
 
 end
