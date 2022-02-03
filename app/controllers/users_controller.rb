04b3c1269ae82cb85684d21631dfea0eaaa96b9f
Jack9145 <toikakunie@gmail.com>
2022-02-03 23:26:55 +0900

    ログイン機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  #CSRF保護の無効化、あとで要確認
+  protect_from_forgery with: :null_session
   def top
   end
 
@@ -8,6 +10,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     @user.save
-    redirect_to("/users/top")
+    redirect_to("/")
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/")
+    else
+      render("users/login")
+    end
   end
 end
