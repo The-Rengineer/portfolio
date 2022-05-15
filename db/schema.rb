62950f86b704528edf88ebba17b6bbe36b0b835f
Ren <ren.12345fox@gmail.com>
2022-05-15 15:54:07 +0900

    schema.rbのエラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220405075104) do
+ActiveRecord::Schema.define(version: 20220426112250) do
 
   create_table "amessages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "message_content", limit: 65535
@@ -28,13 +28,6 @@ ActiveRecord::Schema.define(version: 20220405075104) do
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_chat_messages_on_room_id", using: :btree
     t.index ["user_id"], name: "index_chat_messages_on_user_id", using: :btree
-  end
-
-  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
